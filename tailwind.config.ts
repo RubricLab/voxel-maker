f260d1b4c9198451e173034bc1d20126eb3ad645
tedspare <ted.spare@gmail.com>
2024-11-25 09:54:38 -0500

    Further strip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,3 @@
 import type { Config } from "tailwindcss";
-import rubricConfig from "./rubric.preset";
 
-const tailwindConfig = {
-  content: [
-    "./src/**/*.tsx",
-    "./node_modules/@rubriclab/ui/**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [rubricConfig],
-} satisfies Config;
-
-export default tailwindConfig;
+export default { content: ["./src/**/*.tsx"] } satisfies Config;
